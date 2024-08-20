edcff0943440c7990df266aad2ec0510554a1bda
Shaun Menezes <shaunshanil95@gmail.com>
2024-08-20 14:25:15 +0800

    but svelte-tutorial-2 not there

--- src/app/components/svelte-tutorial-2.jsx
+++ src/app/components/svelte-tutorial-2.jsx
@@ -44,35 +44,6 @@ export default function SvelteTutorial2() {
               Your browser does not support the video tag.
             </video>
           </div>
-
-          {/* <div className="flex justify-center">
-            <ReactPlayer src="/public/intro-bootcamp-video3.mp4" width="100%" />
-          </div> */}
-          {/* <div>
-            <h2 className="text-xl font-semibold">
-              Here are general principles to follow:
-            </h2>
-            <p className="mt-2">
-              1. You don't need to memorise at all. Understanding is the
-              important bit here. This is not your high school exam.
-            </p>{" "}
-            <br />
-            <p>
-              2. Google and Youtube is your best friend. If you are unsure of
-              something, research it there. This is a crucial skill for
-              developers.
-            </p>{" "}
-            <br />
-            <p>
-              3. You learn the most by doing projects, eventhough you don't have
-              enough knowledge to do it yet. The best learning is learning on
-              the fly... trust me
-            </p>
-            <br />
-            <p>
-              4. If you are stuck, that is good. That means you're learning!
-            </p>
-          </div> */}
         </div>
       </div>
     </div>
